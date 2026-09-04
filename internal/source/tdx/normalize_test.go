7cf91e17a65e071f99d63ae5f3adfe4755f869af
yinhm <HaiMing.Yin@gmail.com>
2026-09-04 17:06:05 +0800

    test: cover GBBQ canonical normalization

--- internal/source/tdx/normalize_test.go
+++ internal/source/tdx/normalize_test.go
@@ -1,6 +1,9 @@
 package tdx
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestNormalizeSymbol(t *testing.T) {
 	tests := map[string]struct{ mic, ticker string }{
@@ -36,5 +39,42 @@ func TestActionFromGBBQPreservesETFScale(t *testing.T) {
 	a := ActionFromGBBQ(1, 11, 0, 0, 2.0, 0)
 	if a.ActionType != "scale" || a.ScaleFactor != 2.0 || a.SourceCategory != 11 {
 		t.Fatalf("unexpected action: %#v", a)
+	}
+}
+
+func TestGBBQObservationCreatesVerifiedShareCapital(t *testing.T) {
+	day := time.Date(2026, 6, 30, 15, 0, 0, 0, time.Local)
+	observation, err := GBBQObservation("sh600519", day, 5, 100, 200, 123456789, 200000000, )
+	if err != nil {
+		t.Fatalf("GBBQObservation() error = %v", err)
+	}
+	if observation.Identifier.Value != "sh600519" || observation.Action.ActionDate != day {
+		t.Fatalf("observation identity/date = %#v", observation)
+	}
+	if observation.Action.ActionType != "share_capital_change" || observation.Action.SourceRecordID == "" {
+		t.Fatalf("action = %#v", observation.Action)
+	}
+	if observation.ShareCapital == nil {
+		t.Fatal("expected share-capital observation")
+	}
+	if observation.ShareCapital.FloatShares != 123456789 || observation.ShareCapital.TotalShares != 200000000 {
+		t.Fatalf("share capital = %#v", observation.ShareCapital)
+	}
+	if observation.ShareCapital.SourceRecordID != observation.Action.SourceRecordID {
+		t.Fatalf("source record IDs differ: %q/%q", observation.ShareCapital.SourceRecordID, observation.Action.SourceRecordID)
+	}
+}
+
+func TestGBBQObservationDoesNotInventShareCapitalForUnverifiedCategory(t *testing.T) {
+	day := time.Date(2026, 6, 30, 15, 0, 0, 0, time.Local)
+	observation, err := GBBQObservation("sh600519", day, 6, 1, 2, 3, 4)
+	if err != nil {
+		t.Fatalf("GBBQObservation() error = %v", err)
+	}
+	if observation.Action.ActionType != "new_share_issue" {
+		t.Fatalf("action type = %q", observation.Action.ActionType)
+	}
+	if observation.ShareCapital != nil {
+		t.Fatalf("unexpected share-capital inference: %#v", observation.ShareCapital)
 	}
 }
